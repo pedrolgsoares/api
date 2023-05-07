c29393563bc44a73478ee3daab8ddfa406469548
pedrolgsoares <pedrodevmob@gmail.com>
2023-05-07 15:31:34 -0300

    Migrations com FlyWay

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -16,6 +17,7 @@ public class MedicoController {
     @Autowired //INTERFACE REPOSITORY -> FAZ O CARREGAMENTO REALIZANDO ASSIM A INJEÇÃO DE DEPENDENCIA
     private MedicoRepository repository;
     @PostMapping
+    @Transactional // para uma transação ativa com banco de dados
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
         //          Entidade do tipo JPA Medico que recebe DTO dados
         repository.save(new Medico(dados));
