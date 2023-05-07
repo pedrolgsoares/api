b5ecb2d67b91cbe7c5cdc53766667400ad0b51ea
pedrolgsoares <pedrodevmob@gmail.com>
2023-05-07 14:46:58 -0300

    Adicionando entidades JPA

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,23 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// Entidade JPA
+@Embeddable
+// adicionando anotações lombok para geração de getters e settters e contrutor sem e com argumentos
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
