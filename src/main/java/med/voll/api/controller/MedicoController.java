34d114ea0763383d995b2b9063a8b240676d3cdb
pedrolgsoares <pedrodevmob@gmail.com>
2023-05-19 19:47:03 -0300

    Aplicando funcionalidades de alterações no médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagensMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,5 +32,13 @@ public class MedicoController {
         //diretamente com a ajuda de sua classe. Eles se comportam exatamente como as expressões lambda.
         //A única diferença que tem das expressões lambda é que ela usa referência direta ao método por nome
         //em vez de fornecer um delegado ao método. SINTAXE: <Class name>::<method name>
+    }
+    @PutMapping
+    @Transactional // para uma transação ativa com banco de dados
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        //          Entidade do tipo JPA Medico que recebe DTO dados
+        Medico medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+
     }
 }
